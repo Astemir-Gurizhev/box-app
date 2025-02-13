11be834115b8b129963ba3f3c3a2f5ef2932f6cb
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-13 22:05:17 +0300

    Updated globale styles

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,4 +1,4 @@
 export const russianRequest = 'Ответь на русском. '
 export const clearSymbols =
 	'Без символов меняющих текст. Например: "*", "**", "\n" и т.д. '
-export const task = 'cos(2x)−7sinx−4=0 '
+export const task = ' 2х²+4x+16=0 '
