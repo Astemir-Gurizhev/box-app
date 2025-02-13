23bec66f513ca326f3604ef4445e98d140c44c22
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-13 20:24:17 +0300

    Drawing on the Q&A page

--- src/components/request-form/RequestForm.tsx
+++ src/components/request-form/RequestForm.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { fetchChatResponse } from '../../api/api'
-import { clearSymbols, russianRequest } from '../../data/data'
+import { clearSymbols, russianRequest, task } from '../../data/data'
 
 export const RequestForm = () => {
 	const [response, setResponse] = useState<string | null>(null)
 	const [inputValue, setInputValue] = useState<string>('')
+	const [textQuestion, setTextQuestion] = useState([])
+	const [textResponse, setTextResponse] = useState([])
 
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault()
@@ -15,9 +17,45 @@ export const RequestForm = () => {
 		setResponse(data)
 	}
 
+	const handleCreateQuestions = async () => {
+		const data = await fetchChatResponse(
+			russianRequest +
+				clearSymbols +
+				'Составь актуальные теоретические вопросы по задаче и создай его в виде массива [], где каждый отдельный элемент это один вопрос в виде строки "" и разделены запятыми без начального объяснения' +
+				task
+		)
+
+		setTextQuestion(data)
+	}
+	const handleGettingResponses = async () => {
+		const data = await fetchChatResponse(
+			russianRequest + clearSymbols + 'дай ответы на вопросы и создай его в виде массива [], где каждый отдельный элемент это ответ на один вопрос в виде строки "" и разделены запятыми без начального объяснения' + textQuestion
+		)
+		setTextResponse(data)
+	}
+	console.log(textResponse);
+	
+
 	return (
 		<>
-			<h2>Request Form</h2>
+			<h2>{task}</h2>
+			<button onClick={handleCreateQuestions}>Составить вопросы</button>
+			<div>
+				{textQuestion.length > 0
+					? textQuestion.map((item, i) => {
+							return <p key={i}>{item}</p>
+					  })
+					: ''}
+			</div>
+			<button onClick={handleGettingResponses}>Составить ответы</button>
+			<div>
+				{textResponse.length > 0
+					? textResponse.map((item, i) => {
+							return <p key={i}>{item}</p>
+					  })
+					: ''}
+			</div>
+			{/* <h2>Request Form</h2>
 			<form onSubmit={handleSubmit}>
 				<input
 					type='text'
@@ -27,7 +65,7 @@ export const RequestForm = () => {
 				/>
 				<button type='submit'>Отправить</button>
 			</form>
-			{response && <div>Ответ: {JSON.stringify(response)}</div>}
+			{response && <div>Ответ: {JSON.stringify(response)}</div>} */}
 		</>
 	)
 }
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,2 +1,4 @@
 export const russianRequest = 'Ответь на русском. '
-export const clearSymbols = 'Без символов меняющих текст. Например: "*", "\n" и т.д. '
+export const clearSymbols =
+	'Без символов меняющих текст. Например: "*", "\n" и т.д. '
+export const task = 'x**2 + 2*x + 1 = 0 '
