345385b560de87b0c9d78e92aade79f490c2eece
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-03-09 23:44:13 +0300

    Added route for not found page and updated App component accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import Layout from './layouts/Layout'
 import { Home } from './pages/home/Home'
 import { Second } from './pages/second/Second'
+import NotFound from './pages/not-found/NotFound'
 
 export const App = () => {
 	return (
@@ -11,6 +12,7 @@ export const App = () => {
 					<Route path='/' element={<Layout />}>
 						<Route index path='/' element={<Home />}></Route>
 						<Route index path='/second' element={<Second />}></Route>
+						<Route index path='*' element={<NotFound />}></Route>
 					</Route>
 				</Routes>
 			</BrowserRouter>
---
+++ src/pages/not-found/NotFound.tsx
@@ -0,0 +1,10 @@
+import styles from './NotFound.module.scss'
+const NotFound = () => {
+	return (
+		<div className={styles.notFound}>
+			<h2>Page Not Found</h2>
+		</div>
+	)
+}
+
+export default NotFound
